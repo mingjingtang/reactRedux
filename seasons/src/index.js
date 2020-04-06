3f088a6f70d37633bae9de6cd21cc9dddeed4aef
mingjing tang <tmjcsh@gmail.com>
2020-04-06 18:33:23 -0400

    more annotations for seasons app

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -5,8 +5,12 @@ import Spinner from "./Spinner";
 import "./index.css";
 
 class App extends React.Component {
+  //babel translate below into a this.state in constructor
+  //first thing call by component
   state = { lat: null, errorMessage: "" };
 
+  //usually do data loading here
+  //call one time when content first shows on the screen
   componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
       (position) => this.setState({ lat: position.coords.latitude }),
@@ -27,6 +31,7 @@ class App extends React.Component {
     return <Spinner message="please click the allow!" />;
   }
 
+  //if we have three condition, we can put them into an function
   render() {
     return <div className="border red">{this.renderContent()}</div>;
   }
