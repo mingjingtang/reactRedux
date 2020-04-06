3e3fb28aed8feb4e151ae1a3b6de5bf7b60e5ac2
mingjing tang <tmjcsh@gmail.com>
2020-04-06 18:08:48 -0400

    add some annotation to seasons app

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,40 +1,35 @@
-import React from 'react';
-import ReactDom from 'react-dom';
-import SeasonDisplay from './SeasonDisplay';
-import Spinner from './Spinner'
-import './index.css';
+import React from "react";
+import ReactDom from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
+import "./index.css";
 
+class App extends React.Component {
+  state = { lat: null, errorMessage: "" };
 
-class App extends React.Component{
-    state = {lat: null, errorMessage: ''};
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (err) => this.setState({ errorMessage: err.message })
+    );
+  }
 
-    componentDidMount(){
-        window.navigator.geolocation.getCurrentPosition(
-            position => this.setState({lat: position.coords.latitude}),
-            err => this.setState({errorMessage: err.message})
-        );
+  //conditional rendering
+  //use state as prop
+  renderContent() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+    if (!this.state.errorMessage && this.state.lat) {
+      return <SeasonDisplay lat={this.state.lat} />;
     }
 
-    renderContent(){
-        if(this.state.errorMessage && ! this.state.lat){
-            return <div>Error: {this.state.errorMessage}</div>
-        }
-        if(!this.state.errorMessage && this.state.lat){
-            return <SeasonDisplay
-                        lat = {this.state.lat}
-                    />
-        }
+    return <Spinner message="please click the allow!" />;
+  }
 
-        return <Spinner message = "please click the allow!"/>;
-    }
-
-    render(){
-        return (
-            <div className="border red">
-                {this.renderContent()}
-            </div>
-        );
-    }
+  render() {
+    return <div className="border red">{this.renderContent()}</div>;
+  }
 }
 
-ReactDom.render(<App />, document.querySelector('#root'));
+ReactDom.render(<App />, document.querySelector("#root"));
